824154a57a09200dc1440b589c3cee00c8d27288
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2024-05-26 22:58:21 +0500

    (fix): image file id

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ BOT_TOKEN = env.str("BOT_TOKEN")
 BACKEND_URL = env.str("BACKEND_URL")
 WEB_APP_URL = env.str("WEB_APP_URL")
 GROUP_ID = env.str("GROUP_ID")
+IMAGE_FILE_ID = env.str("IMAGE_FILE_ID")
 
 ALLOWED_HOSTS = env.str("ALLOWED_HOSTS", "").split(",")
 CSRF_TRUSTED_ORIGINS = env.str("CSRF_TRUSTED_ORIGINS", "").split(",")
